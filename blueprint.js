17ac7e4c5d56963e000d321b3165c94fd366ee3f
codejvn <65090247+codejvn@users.noreply.github.com>
2022-02-20 14:33:10 -0500

    updated changes

---
+++ blueprint.js
@@ -0,0 +1,11 @@
+let marvelHeroes = [["Iron Man", 75], ["Captain America", 80], ["Scarlet Witch", 100], ["Thor", 100], ["Spider Man", 90], ["Quick Silver", 60], ["Wolverine", 92], 
+["Hulk", 95], ["Deadpool", 92], ["Black Panther", 85], ["Doctor Strange", 98], ["Hawkeye", 50], ["Captain Marvel", 100], ["Vision", 90], ["Falcon", 55], ["War Machine", 73], 
+["Black Widow", 50],["Ant Man", 72], ["Groot", 65], ["Drax", 55]];
+
+let dcHeroes = [["Superman", 100], ["Batman", 78], ["Wonder Woman", 93], ["Flash", 100], ["Aquaman", 83], ["Cyborg", 65], ["Green Lantern", 67]
+["Shazam", 90], ["Green Arrow", 59], ["Martian Manhunter", 82], ["Nightwing", 60], ["Robin", 57], ["Batwoman", 61], ["Supergirl", 87], ["Red Hood", 62], ["Blue Beetle", 65], ["Starfire", 85], 
+["Raven", 89], ["Beast Boy", 70], ["Superboy", 87]];
+
+function generateTeam(){
+    Math.floor(Math.random())
+}
